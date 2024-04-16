8a8e03d39a9621ed0a36ced634d31e747f4361a9
young <55731744@qq.com>
2024-04-16 14:54:45 +0800

    更改打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,17 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
+    // minify: 'terser',
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'PanZoomControl',
       // the proper extensions will be added
-      fileName: 'PanZoomControl'
+      fileName: (format) => format === 'umd' ? `PanZoomControl.${format}.js` : 'PanZoomControl.min.js',
+      formats: ['iife', 'umd']
+    },
+    terserOptions: {
+      mangle: true
     }
   }
 })
